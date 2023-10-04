d0f1b0c0c161a881faabd9be9cca95a5bd884f75
Zohra Serhenova <78276178+zohraserhenova@users.noreply.github.com>
2023-10-04 18:04:30 +0300

    Add files via upload

---
+++ Runner Game.py
@@ -0,0 +1,48 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption("Runner")
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('Python Projects\Runner\gfont\Pixeltype.ttf', 50)
+
+sky_surface = pygame.image.load('Python Projects\Runner\graphics\sky.png').convert()
+ground_surface = pygame.image.load('Python Projects\Runner\graphics\ground.png').convert()
+
+score_surface = test_font.render('My Game', False, 'Black')
+score_rectangle = score_surface.get_rect(center = (400,50))
+
+snail_surface = pygame.image.load('Python Projects\Runner\graphics\snail1.png').convert_alpha()
+snail_rectangle = snail_surface.get_rect(bottomright = (600,300))
+
+player_surface = pygame.image.load('Python Projects\Runner\graphics\player_walk_1.png').convert_alpha()
+player_rectangle = player_surface.get_rect(midbottom = (80,300))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        # if event.type == pygame.MOUSEMOTION:
+            # if player_rectangle.collidepoint(event.pos):
+                # print('collision')
+    
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(score_surface, score_rectangle)
+    
+    snail_rectangle.x -= 4
+    if snail_rectangle.right <= 0:
+        snail_rectangle.left = 800
+    screen.blit(snail_surface, snail_rectangle)
+    screen.blit(player_surface, player_rectangle)
+    # if player_rectangle.colliderect(snail_rectangle):
+    #     print('collision')
+
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rectangle.collidepoint(mouse_pos):
+    #     print(pygame.mouse.get_pressed())
+    
+    pygame.display.update()
+    clock.tick(60)
